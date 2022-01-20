937d3733119fbc1709114e55ecbe6ce87f8e0abd
Wasim <wasim.rashid@developers.studio>
2022-01-20 12:03:21 +0500

    test laravel notifications

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\MyFirstNotification;
+use App\User;
 use Illuminate\Http\Request;
+use Notification;
 
 class HomeController extends Controller
 {
@@ -25,6 +28,23 @@ class HomeController extends Controller
     {
 //        remove extra commits
         return view('home');
+    }
+
+    public function sendNotification()
+    {
+        $user = User::first();
+        $details = [
+            'greeting' => 'Hi Artisan',
+            'body' => 'This is my first notification from ItSolutionStuff.com',
+            'thanks' => 'Thank you for using ItSolutionStuff.com tuto!',
+            'actionText' => 'View My Site',
+            'actionURL' => url('/'),
+            'order_id' => 101
+        ];
+
+        Notification::send($user, new MyFirstNotification($details));
+
+        dd('done');
     }
 
     public function downloadDailyStatementWorking(Request $request)
